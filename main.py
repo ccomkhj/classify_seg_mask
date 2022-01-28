98c3f3754cfb26eaa792cfed4d52d7f935c297b6
ccomkhj <ccomkhj@gmail.com>
2022-01-28 16:25:43 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,194 @@
+from __future__ import print_function
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.optim.lr_scheduler import StepLR
+import argparse
+from tools.builder import SymbolDataset, set_files
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+from torch.utils.data import DataLoader
+import cv2
+from loguru import logger
+
+
+class Net(nn.Module):
+    def __init__(self, out_ch):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels = 4, out_channels = 32, kernel_size = 7, stride= 1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size = 7, stride = 1)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, out_ch) # it should be the number of class !
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+
+
+def train(args, model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+            if args.dry_run:
+                break
+
+
+def test(model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Train a model')
+    parser.add_argument('--train_path', default='images/train', help='the dir to training images')
+    parser.add_argument('--test_path', default='images/test', help='the dir to testing images')
+
+    parser.add_argument('--batch_size', type=int, default=4, metavar='N',
+                        help='input batch size for training (default: 64)')
+    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                        help='input batch size for testing (default: 1000)')
+    parser.add_argument('--epochs', type=int, default=14, metavar='N',
+                        help='number of epochs to train (default: 14)')
+    parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
+                        help='learning rate (default: 1.0)')
+    parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
+                        help='Learning rate step gamma (default: 0.7)')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    parser.add_argument('--dry-run', action='store_true', default=False,
+                        help='quickly check a single pass')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                        help='how many batches to wait before logging training status')
+    parser.add_argument('--save-model', action='store_true', default=False,
+                        help='For Saving the current Model')
+                        
+
+    args = parser.parse_args()
+    return args
+
+def main():
+    # Training settings
+    args = parse_args()
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+
+    torch.manual_seed(args.seed)
+
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    train_kwargs = {'batch_size': args.batch_size}
+    test_kwargs = {'batch_size': args.test_batch_size}
+    if use_cuda:
+        cuda_kwargs = {'num_workers': 1,
+                       'pin_memory': True,
+                       'shuffle': True}
+        train_kwargs.update(cuda_kwargs)
+        test_kwargs.update(cuda_kwargs)
+
+    #######################################################
+    #               Define Transforms
+    #######################################################
+
+    #To define an augmentation pipeline, you need to create an instance of the Compose class.
+    #As an argument to the Compose class, you need to pass a list of augmentations you want to apply. 
+
+    train_transforms = A.Compose([
+            A.Rotate(limit= [-180,180], border_mode= cv2.BORDER_CONSTANT, value=0, mask_value= 1, p=1),  # mask value could be 255. It should be verified.,
+            A.Resize (height=100, width=100, p=1),
+            # A.SmallestMaxSize(max_size=350), # Rescale an image so that minimum side is equal to max_size, keeping the aspect ratio of the initial image.
+            # A.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.05, rotate_limit=360, p=0.5),
+            # A.RandomCrop(height=256, width=256),
+            # A.RGBShift(r_shift_limit=15, g_shift_limit=15, b_shift_limit=15, p=0.5),
+            # A.RandomBrightnessContrast(p=0.5),
+            # A.MultiplicativeNoise(multiplier=[0.5,2], per_channel=True, p=0.2),
+            # A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+            # A.HueSaturationValue(hue_shift_limit=0.2, sat_shift_limit=0.2, val_shift_limit=0.2, p=0.5),
+            # A.RandomBrightnessContrast(brightness_limit=(-0.1,0.1), contrast_limit=(-0.1, 0.1), p=0.5),
+            ToTensorV2(),
+        ])
+
+    test_transforms = A.Compose([
+            # A.SmallestMaxSize(max_size=350),
+            # A.CenterCrop(height=256, width=256),
+            # A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+            A.Resize (height=200, width=200, p=1),
+            ToTensorV2(),
+        ])
+
+    train_image_paths, valid_image_paths, test_image_paths, class_to_idx  = set_files(args.train_path, args.test_path)
+
+    #######################################################
+    #                  Create Dataset
+    #######################################################
+
+    train_dataset = SymbolDataset(train_image_paths, class_to_idx, train_transforms)
+    valid_dataset = SymbolDataset(valid_image_paths, class_to_idx, test_transforms) #test transforms are applied
+    test_dataset = SymbolDataset(test_image_paths, class_to_idx, test_transforms)
+
+    logger.info('The shape of tensor for the first image in train dataset: ',train_dataset[0][0].shape)
+    logger.info('The label for the first image in train dataset: ',train_dataset[0][1])
+
+    #######################################################
+    #                  Define Dataloaders
+    #######################################################
+
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
+    valid_loader = DataLoader(valid_dataset, batch_size=args.batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False)
+
+    num_class = len(class_to_idx)
+    model = Net(num_class).to(device)
+    optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
+
+    scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+    for epoch in range(1, args.epochs + 1):
+        train(args, model, device, train_loader, optimizer, epoch)
+        test(model, device, test_loader)
+        scheduler.step()
+
+    if args.save_model:
+        torch.save(model.state_dict(), "test.pt")
+
+
+if __name__ == '__main__':
+    main()
---
+++ tools/builder.py
@@ -0,0 +1,118 @@
+from torch.utils.data import Dataset
+
+import cv2
+import glob
+import numpy as np
+import random
+import copy
+
+from loguru import logger
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('TkAgg')
+
+
+
+
+def flatten(t):
+    return [item for sublist in t for item in sublist]
+
+####################################################
+#       Create Train, Valid and Test sets
+####################################################
+def set_files(train_data_path, test_data_path):
+    # train_data_path = 'images/train' 
+    # test_data_path = 'images/test'
+
+    train_image_paths = [] #to store image paths in list
+    classes = [] #to store class values
+
+    #1.
+    # get all the paths from train_data_path and append image paths and class to to respective lists
+    # eg. train path-> 'images/train/26.Pont_du_Gard/4321ee6695c23c7b.jpg'
+    # eg. class -> 26.Pont_du_Gard
+    for data_path in glob.glob(train_data_path + '/*'):
+        classes.append(data_path.split('/')[-1]) 
+        train_image_paths.append(glob.glob(data_path + '/*'))
+        
+    train_image_paths = list(flatten(train_image_paths))
+    random.shuffle(train_image_paths)
+
+    logger.info('train_image_path example: ', train_image_paths[0])
+    logger.info(f'classes: {classes}')
+
+    #2.
+    # split train valid from train paths (80,20)
+    train_image_paths, valid_image_paths = train_image_paths[:int(0.8*len(train_image_paths))], train_image_paths[int(0.8*len(train_image_paths)):] 
+
+    #3.
+    # create the test_image_paths
+    test_image_paths = []
+    for data_path in glob.glob(test_data_path + '/*'):
+        test_image_paths.append(glob.glob(data_path + '/*'))
+
+    test_image_paths = list(flatten(test_image_paths))
+
+    logger.info("Train size: {}, Valid size: {}, Test size: {}".format(len(train_image_paths), len(valid_image_paths), len(test_image_paths)))
+
+    #######################################################
+    #      Create dictionary for class indexes
+    #######################################################
+
+    idx_to_class = {i:j for i, j in enumerate(classes)}
+    class_to_idx = {value:key for key,value in idx_to_class.items()}
+
+    return train_image_paths, valid_image_paths, test_image_paths, class_to_idx 
+
+
+
+#######################################################
+#               Define Dataset Class
+#######################################################
+
+class SymbolDataset(Dataset):
+    def __init__(self, image_paths, class_to_idx, transform=False):
+        self.image_paths = image_paths
+        self.transform = transform
+        self.class_to_idx = class_to_idx 
+        
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        image_filepath = self.image_paths[idx]
+        image = cv2.imread(image_filepath)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # if input is gray, then it should be changed, too.
+        
+        label = image_filepath.split('/')[-2]
+        label = self.class_to_idx[label]
+        if self.transform is not None:
+            image = self.transform(image=image)["image"]
+        
+        return image, label
+    
+#######################################################
+#                  Visualize Dataset
+#         Images are plotted after augmentation
+#######################################################
+
+def visualize_augmentations(dataset, idx=0, samples=10, cols=5, random_img = False):
+    
+    dataset = copy.deepcopy(dataset)
+    #we remove the normalize and tensor conversion from our augmentation pipeline
+    dataset.transform = A.Compose([t for t in dataset.transform if not isinstance(t, (A.Normalize, ToTensorV2))])
+    rows = samples // cols
+    
+        
+    figure, ax = plt.subplots(nrows=rows, ncols=cols, figsize=(12, 8))
+    for i in range(samples):
+        if random_img:
+            idx = np.random.randint(1,len(train_image_paths))
+        image, lab = dataset[idx]
+        ax.ravel()[i].imshow(image)
+        ax.ravel()[i].set_axis_off()
+        ax.ravel()[i].set_title(idx_to_class[lab])
+    plt.tight_layout(pad=1)
+    plt.show()    
+
+# visualize_augmentations(train_dataset, np.random.randint(1,len(train_image_paths)), random_img = True)
